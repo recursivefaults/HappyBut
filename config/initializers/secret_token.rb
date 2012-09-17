c7c8512d0f92cd8520bd6f7c08e8645fc7a36f1b
Ryan Latta <recursive.faults@gmail.com>
2012-09-17 18:31:47 -0400

    Initial commit with dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyBut::Application.config.secret_token = '93aca9362aacb0545a3c9a5f352fa37e3c461d4ef7e36ed1626d6af0a8407314eb78c1a60b2c64144c10db4417bbb77d14210618cda0be1bdc62a3f706a7ec88'
